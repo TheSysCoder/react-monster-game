cfcb61b84ced1deb01975ea46259cc019b9fef0d
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-06-20 19:10:28 +0530

    Promises

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 
@@ -6,25 +5,27 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Jake",
-      company: "Microsoft",
+      monsters: [],
     };
+  }
+  // fetching the monster names from API
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => {
+        this.setState(() => {
+          return {
+            monsters: users,
+          };
+        });
+      })
+    );
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hii, {this.state.name}</p>
-          <p>I am working at {this.state.company}</p>
-          <button
-            onClick={() => {
-              this.setState({ name: "Jerry" });
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
