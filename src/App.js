0e54ad92bc1acca03dd3ef65f7a9ff4f1652609f
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-06-23 20:09:18 +0530

    backend done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React, { Component } from "react";
-
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
   // fetching the monster names from API
@@ -15,33 +17,30 @@ class App extends Component {
         this.setState(() => {
           return {
             monsters: users,
-            searchField: "",
           };
         });
       })
     );
   }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <div className="App">
-        <input
-          type="text"
-          className="search-box"
-          placeholder="Search monster"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
-        />
-        {filteredMonsters.map((monster) => {
+        <SearchBox onChangeHandler={onSearchChange} />
+        {/* {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
+        })} */}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
